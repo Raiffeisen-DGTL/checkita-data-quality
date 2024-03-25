bc8bfd8f52d20d0c78adf449afdcfac550febf32
Ermolaev Dmitry <89921988+dmrmlvv@users.noreply.github.com>
2024-03-25 19:26:19 +0300

    fix: update JobConfig and added test for config encryptor (#34)
    - added description and metadata in job config
    - added a test for the correct conversion after encryption

--- checkita-core/src/test/scala/ru/raiffeisen/checkita/config/IOSpec.scala
+++ checkita-core/src/test/scala/ru/raiffeisen/checkita/config/IOSpec.scala
@@ -1,8 +1,12 @@
 package ru.raiffeisen.checkita.config
 
+import com.typesafe.config.ConfigRenderOptions
+import eu.timepit.refined.api.Refined
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
-import ru.raiffeisen.checkita.config.IO.{readJobConfig, writeJobConfig}
+
+import ru.raiffeisen.checkita.config.IO.{readEncryptedJobConfig, readJobConfig, writeEncryptedJobConfig, writeJobConfig}
+import ru.raiffeisen.checkita.config.Parsers.StringConfigParser
 
 import java.io.File
 
@@ -14,7 +18,25 @@ class IOSpec extends AnyWordSpec with Matchers {
       val written = readJobConfig[File](configFile).flatMap(config => writeJobConfig(config)).map(_ => "Success")
 
       written shouldEqual Right("Success")
-      
+    }
+
+    "be decrypted successfully" in {
+      val encryptor = new ConfigEncryptor(
+        Refined.unsafeApply("secretmustbeatleastthirtytwocharacters"), Seq("password")
+      )
+
+      val renderOpts = ConfigRenderOptions.defaults().setComments(false).setOriginComments(false).setFormatted(false)
+      val configFile = new File(getClass.getResource("/test_job.conf").getPath)
+
+      val written = readJobConfig[File](configFile).right.get
+
+      val encryptedConfig = writeEncryptedJobConfig(written)(encryptor).right.get.root().render(renderOpts)
+      val decryptedConfig = readEncryptedJobConfig[String](encryptedConfig)(StringConfigParser, encryptor).right.get
+
+      val originalConfig = writeJobConfig(written).right.get.root().render(renderOpts)
+      val afterDecryption = writeJobConfig(decryptedConfig).right.get.root().render(renderOpts)
+
+      originalConfig shouldEqual afterDecryption
     }
   }
 }
